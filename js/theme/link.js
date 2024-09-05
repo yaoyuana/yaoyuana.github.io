89716ac7c6afc2e12d52c68db2e97afbc7889863
Yao.Yuan <yao.yuan@advantech.com.cn>
2024-09-05 18:16:25 +0800

    Site updated: 2024-09-05 18:16:25

--- js/theme/link.js
+++ js/theme/link.js
@@ -26,22 +26,6 @@ fetch('/links/linklist.json')
   .then(res => renderlink(res));
   
 window.onload = function() {
-  // new Valine({
-  //     el: '#vcomment',
-  //     notify: true,
-  //     verify: false,
-  //     // app_id: 'DFIWx0Ig1P6q1pCANzVk1rOp-MdYXbMMI',
-  //     // app_key: 'LNfCXusAIdFZmfswnwN3a0gX',
-  //     app_id: 'unnudWlPyrKXJy8ZHA7NZclo-gzGzoHsz',
-  //     app_key: 'HAVxxg2rAU74oBO1M1ssTo1U',
-  //     placeholder: '留下点什么吧......',
-  //     avatar: 'monsterid',
-  //     pageSize: 5,
-  //     visitor: true,
-  //     highlight: true,
-  //     recordIP: true,
-  //     // serverURLs: 'https://dfiwx0ig.api.lncldglobal.com'  // 增加这一行！！！
-  // });
     twikoo.init({
       envId: 'https://twikoo.yaoyuan.vip/.netlify/functions/twikoo', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
       el: '#tcomment', // 容器元素
